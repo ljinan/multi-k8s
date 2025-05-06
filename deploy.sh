5b6d41fe2e709276bdd92edcae8858ca908f3554
jinan <lohjinan1996@gmail.com>
2025-05-07 01:29:42 +0800

    updated files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
-docker build -t ljinan/multi-client -f ./client/Dockerfile ./client
-docker build -t ljinan/multi-server -f ./server/Dockerfile ./server
-docker build -t ljinan/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t ljinan/multi-client:latest -t ljinan/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ljinan/multi-server:latest -t ljinan/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ljinan/multi-worker:latest -t ljinan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# tagging the docker image with 2 tags, latest and SHA number. 
+# helps to allow for debugging. -> app is breaking -> check which image is the deployment running (image:SHA #)
+# -> git checkout SHA# (revert codebase back to that particular commit) -> debug the app
+docker push ljinan/multi-client:latest
+docker push ljinan/multi-server:latest
+docker push ljinan/multi-worker:latest
+
+docker push ljinan/multi-client:$SHA
+docker push ljinan/multi-server:$SHA
+docker push ljinan/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ljinan/multi-server:$SHA
+kubectl set image deployments/cilent-deployment client=ljinan/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ljinan/multi-worker:$SHA
